ee8c35a39e172cc2ade2c8d2e1324ec32c2a6cea
Adrians <adrianspolis@Opti-05.localdomain>
2026-02-26 11:38:50 +0200

    Create blade and BookController and route created

---
+++ resources/views/Books/create.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forma</title>
+</head>
+<body>
+    <h1>Grāmatas forma</h1>
+ <form>
+  <label for="title">Title:</label><br>
+  <input type="text" id="title" name="title"><br>
+  <label for="author">Author:</label><br>
+  <input type="text" id="author" name="author">
+</form>
+</body>
+</html>
